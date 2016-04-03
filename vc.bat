df89411514be64dbc37b26ae0448f171ef020317
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-04-03 09:59:23 +0900

    fix %0

--- vc.bat
+++ vc.bat
@@ -2,7 +2,7 @@
 
 setlocal
 set name=%~n0
-set ver=%name:vc=%
+set ver=%name:vc=%0
 for /f "usebackq delims== tokens=1*" %%I in (`set VS%ver%COMNTOOLS`) do (
     if "%%I" == "VS%ver%COMNTOOLS" set vsdir=%%J
 )
