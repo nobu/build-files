e8e439f70cad8b1bc32faa97c65c325d8ae237c8
Nobuyoshi Nakada <nobu@ruby-lang.org>
2017-08-23 13:28:46 +0900

    Ignore unavaiable VCS

--- Ruby.mk
+++ Ruby.mk
@@ -88,6 +88,10 @@ SRCS := $(call cvs_srcs) $(call cvs_srcs,missing/) $(call cvs_srcs,win32/)
 else
 SRCS := $(wildcard $(srcdir_prefix)*.h $(filter-out $(srcdir_prefix)parse.c,$(srcdir_prefix)*.c) $(srcdir_prefix)parse.y $(srcdir_prefix)missing/*.[ch] $(srcdir_prefix)win32/win32.[ch] $(srcdir_prefix)win32/dir.h)
 endif
+ifeq ($(if $(VCS),$(shell hash $(VCS) 2>/dev/null),none),)
+VCS :=
+endif
+
 TESTS ?= $(if $(wildcard .tests),$(shell cat .tests),$(EXTS))
 
 VCSUP ?= $(VCS) $(call or,$(value $(subst  ,-,$(VCS))up),up) $(value $(subst  ,-,$(VCS))up-options)
