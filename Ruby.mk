bf66eb0e20ad89ae8e22950429a6421a8d2d1151
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-04-04 12:25:05 +0900

    Push to all if ci-skip

--- Ruby.mk
+++ Ruby.mk
@@ -384,7 +384,8 @@ last-pr:
 
 .do-up: $(before-up) prev-head last-pr
 	$(call or,$(in-srcdir),env) LC_TIME=C $(VCSUP)
-	git -C $(srcdir) fetch github
+	@if git log -1 | grep -F '[ci skip]' > /dev/null; then git push all; fi
+	git -C $(srcdir) fetch usual
 	$(if $(POST_UP1),-$(call or,$(in-srcdir),env) LC_TIME=C $(POST_UP1))
 	$(if $(POST_UP2),-$(call or,$(in-srcdir),env) LC_TIME=C $(POST_UP2))
 	$(if $(filter $(srcdir_prefix)revision.h,$(prereq-targets)),,-@$(RM) $(srcdir_prefix)revision.h)
