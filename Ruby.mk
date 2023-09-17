a5d8439093397b046eb04aed037a771704ca04ea
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-09-18 00:02:04 +0900

    Clean destdir then install

--- Ruby.mk
+++ Ruby.mk
@@ -293,8 +293,13 @@ $(1)/%: prereq .force
 	+$$(submake) TOPMAKE=$(value TOPMAKE) $$(mflags) $$(@F)
 
 $(1)/inst: .force
-	@{ echo include GNUmakefile; echo 'install-everything: clean-docs install; $$$$(CP) $$$$(INSTALLED_LIST) $$$$(DESTDIR)/'; } | \
-	    $$(MAKE) -C $$(@D) -f - prereq-targets= install-everything INSTRUBY_OPTS='--install=dbg --debug-symbols=dSYM'
+	@{ \
+	  echo include GNUmakefile; \
+	  echo 'install-everything: clean-docs install; $$$$(CP) $$$$(INSTALLED_LIST) $$$$(DESTDIR)/'; \
+	  echo 'install: clean-destdir'; \
+	  echo 'clean-destdir:; -$$$$(Q) $$$$(RMALL) $$$$(DESTDIR)/'; \
+	} | \
+	$$(MAKE) -C $$(@D) -f - prereq-targets= install-everything INSTRUBY_OPTS='--install=dbg --debug-symbols=dSYM'
 endef
 $(foreach subdir,$(subdirs),$(eval $(call subdircmd,$(subdir))))
 
